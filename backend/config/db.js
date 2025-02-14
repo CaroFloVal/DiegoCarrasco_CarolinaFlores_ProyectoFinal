b45394c22651aa8ee21e59018a602ba4c7328728
Diego Carrasco <diegocarrasco.ordonez@gmail.com>
2025-02-14 18:41:58 -0300

    Fix import Pool in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,13 @@
+const { Pool } = require('pg'); 
+require('dotenv').config();
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }  
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }  // 🔹 Para conexiones seguras en Render
 });
+
+module.exports = pool;
