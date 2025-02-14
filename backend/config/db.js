6e2dca41a1aa603296bb6b566e7dd102dc5cf724
Diego Carrasco <diegocarrasco.ordonez@gmail.com>
2025-02-14 17:42:06 -0300

    nuevo cambio en db para render

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,8 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false, 
-  },
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false, // Corrección aquí
 });
-
-pool.connect()
-  .then(() => console.log('✅ Conectado a la base de datos en Render'))
-  .catch(err => console.error('❌ Error al conectar con la base de datos:', err));
-
-module.exports = pool;
